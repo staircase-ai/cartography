cbe147383a507043f27f092962018c45f012713e
itaim <itai@staircase.ai>
2022-06-10 08:30:35 +0300

    add setup and toml

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='cartography',
+    packages=['cartography','configs'],
+    version='0.1.0',
+    description='Fork from allenai/cartography ',
+    author='itai@staircase.ai',
+    url='https://github.com/staircase-ai/cartography.git',
+    license='MIT'
+)
+
+#install_requires=['pandas','transformers','torch','scikit-learn','boto3','fastt5','cdifflib'],
+#tests_require=['pytest','unittest'],
+#test_suite='tests',
